1b2ce80a57b066ce6f618783da245907fd258985
Ewelina <ewelinazawol@gmail.com>
2022-06-21 13:57:55 +0100

    Transform functional components to class components

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -1,21 +1,27 @@
+import React, { Component } from 'react';
 import './Track.css';
 
-export function Track(){
-    return (
-        <div className="Track">
-            <div className="Track-information">
-              {/* <h3><!-- track name will go here --></h3> */}
-              {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+export class Track extends Component{
+    constructor(props){
+        super(props);
+        this.renderAction = this.renderAction.bind(this);
+    }
+    renderAction(){
+        if (this.props.isRemoval) {
+            '+'
+        } else {
+            '-'
+        }
+    }
+    render(){
+        return (
+            <div className="Track">
+                <div className="Track-information">
+                  {/* <h3><!-- track name will go here --></h3> */}
+                  {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+                </div>
+                <button className="Track-action">{this.renderAction}</button>
             </div>
-            <button className="Track-action">{renderAction}</button>
-        </div>
-    );
-}
-
-function renderAction(props) {
-    if (props.isRemoval) {
-        '+'
-    } else {
-        '-'
+        );
     }
 }
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,9 +1,12 @@
+import React, { Component } from 'react';
 import './TrackList.css';
 
-export function TrackList() {
-    return(
-        <div className="TrackList">
-            {/* <!-- You will add a map method that renders a set of Track components  --> */}
-        </div>
-    );
+export class TrackList extends Component {
+    render(){
+        return(
+            <div className="TrackList">
+                {/* <!-- You will add a map method that renders a set of Track components  --> */}
+            </div>
+        );
+    }
 }
